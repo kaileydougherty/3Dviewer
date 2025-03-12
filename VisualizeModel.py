8de30d9b70fdf0f3170d0ef955e67577e8d0b53c
Kailey Dougherty <kaileydougherty@mines.edu>
2025-03-12 12:22:05 -0600

    Check if plot object is list, valid

--- VisualizeModel.py
+++ VisualizeModel.py
@@ -1,7 +1,7 @@
 # Create a 3D visualization for distributed fiber optic sensing data for microseismic events.
 # Author: Kailey Dougherty
 # Date created: 24-FEB-2025
-# Date last modified: 09-MAR-2025
+# Date last modified: 12-MAR-2025
 
 # Import needed libraries
 import pandas as pd
@@ -19,7 +19,20 @@ class DataViewer:
 		fig = go.Figure()
 
 		for b in self.plot_objects:
-			fig.add_trace(b)
+			# Check if the object is a list
+			if isinstance(b, list):
+				for c in b:
+					if isinstance(c, go.Scatter3d):  #If yes, add as trace
+						fig.add_trace(c)
+					else:  #Otherwise, print error message
+						print(f"Invalid object: {c}")
+			else:			
+				# Check if the object is valid
+				if isinstance(b, go.Scatter3d):  #If yes, add as trace
+					fig.add_trace(b)
+				else:  #Otherwise, print error message
+					print(f"Invalid object: {b}")
+
 	
 		fig.update_layout()
 		

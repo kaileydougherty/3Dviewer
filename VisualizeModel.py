30cecd413b5a3fd8315a9c1bb8ff36f4f60d0c14
Kailey Dougherty <kaileydougherty@mines.edu>
2025-03-16 13:22:09 -0500

    Adjust layout, traces for plot objects

--- VisualizeModel.py
+++ VisualizeModel.py
@@ -1,7 +1,7 @@
 # Create a 3D visualization for distributed fiber optic sensing data for microseismic events.
 # Author: Kailey Dougherty
 # Date created: 24-FEB-2025
-# Date last modified: 12-MAR-2025
+# Date last modified: 16-MAR-2025
 
 # Import needed libraries
 import pandas as pd
@@ -9,6 +9,10 @@ import numpy as np
 import os
 import plotly.graph_objects as go
 
+# NEXT TASKS:
+# (1) Take BEG and END times from all objects and combine
+# (2) Define scroll bar (JUST APPEAR BETWEEN, NOT MOVIE) - take well log as just end snapshot
+# (3) Depending on type of go., make IF branching in .draw()'''
 
 class DataViewer:
 	
@@ -18,22 +22,28 @@ class DataViewer:
 	def	draw(self):
 		fig = go.Figure()
 
-		for b in self.plot_objects:
-			# Check if the object is a list
+		for b in self.plot_objects:	
 			if isinstance(b, list):
 				for c in b:
 					if isinstance(c, go.Scatter3d):  #If yes, add as trace
 						fig.add_trace(c)
+
 					else:  #Otherwise, print error message
 						print(f"Invalid object: {c}")
-			else:			
+			else:	
 				# Check if the object is valid
 				if isinstance(b, go.Scatter3d):  #If yes, add as trace
 					fig.add_trace(b)
+
 				else:  #Otherwise, print error message
 					print(f"Invalid object: {b}")
-
 	
-		fig.update_layout()
+		fig.update_layout(
+		 	legend=dict(
+                x=0.8,
+                y=0.9,
+                xanchor='left',
+                yanchor='top'
+        ))
 		
 		return fig.show()
